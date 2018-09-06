4c041b1c43a15909b46b51c439a195f1d2903a34
Artem Pinchuk <artem092@mail.ru>
2018-09-06 23:16:44 +1000

    Added initial display support.

--- mqtt_boiler.ino
+++ mqtt_boiler.ino
@@ -1,5 +1,11 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include <cmath>
 
 /* HW history
  *  0.1 - Initial HW for testing
@@ -16,10 +22,15 @@
 // ***** CONFIG *****
 // Hardware configuration
 #define ONE_WIRE_BUS 2
+#define OLED_DC 4
+#define OLED_CS 15
+#define OLED_RES 5
 // ***** END OF CONFIG *****
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+
+Adafruit_SSD1306 display(OLED_DC, OLED_RES, OLED_CS);
 
 float gTemperature;
 
@@ -34,14 +45,35 @@ float updateTemperature() {
   Serial.println(gTemperature);
 }
 
+void updateDisplay() {
+  int8 rounded = (int8)round(gTemperature);
+
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.println(rounded);
+  display.setTextSize(2);
+  display.setCursor(10,40);
+  display.println(rounded);
+  display.setTextSize(3);
+  display.setCursor(74,40);
+  display.println(rounded);
+
+  display.display();
+}
+
 void setup(void) {
   Serial.begin(9600);
   Serial.println(DEVICE_NAME " HW Ver. " DEVICE_HW_VERSION " SW Ver. " DEVICE_SW_VERSION);
 
   sensors.begin();
+  display.begin(SSD1306_SWITCHCAPVCC);
+  display.clearDisplay();
 }
 
 void loop(void) {
   updateTemperature();
+  updateDisplay();
 }
 
